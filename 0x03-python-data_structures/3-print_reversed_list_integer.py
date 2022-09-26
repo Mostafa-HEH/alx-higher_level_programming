1d8377d9157a639cfe6513ff5ee481bceb4a01fe
Mostafa Hassan <mostafa813765@gmail.com>
2022-09-26 23:49:05 +0000

    add checker for type of input

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
     list_length = len(my_list)
-    my_list.reverse()
-    for i in range(list_length):
-        print('{:d}'.format(my_list[i]))
+    if isinstance(my_list, list):
+        my_list.reverse()
+        for i in range(list_length):
+            print('{:d}'.format(my_list[i]))
