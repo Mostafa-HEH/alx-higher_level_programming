fcea1d910750b30f96eba613fce5752759815ba7
Mostafa Hassan <mostafa813765@gmail.com>
2022-09-26 23:18:02 +0000

    length fix

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -2,6 +2,6 @@
 def element_at(my_list, idx):
     listLength = len(my_list)
 
-    if(idx < 0) or (idx > listLength):
+    if(idx < 0) or (idx > listLength - 1):
         return None
     return my_list[idx]
