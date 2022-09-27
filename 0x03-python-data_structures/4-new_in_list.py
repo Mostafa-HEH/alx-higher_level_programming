b40ba5b9ea51089703b42c0682e0f7899a567097
Mostafa Hassan <mostafa813765@gmail.com>
2022-09-27 15:57:25 +0000

    fix for checker

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    modified_list = [x for x in my_list]
-    # If idx is negative or out of range, return a copy of the original list
-    if (idx >= 0) or (idx < len(my_list)):
-        modified_list[idx] = element
-        return modified_list
-    return my_list
 
+    modified_list = [x for x in my_list]
+
+    # If idx is negative or out of range, return a copy of the original list
+    if (idx < 0) or (idx > (len(my_list) - 1)):
+        return my_list
+
+    modified_list[idx] = element
+    return modified_list
