beda152f3f65a7bb2ce1fa74b657b47da9dd5291
Mostafa Hassan <mostafa813765@gmail.com>
2022-09-27 16:58:56 +0000

    fix for checker

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,4 +1,4 @@
 #!/usr/bin/env python3
 def no_c(my_string):
-    modefied_string = [x for x in my_string if not(x == "c" or x == "C")]
+    modefied_string = [x for x in my_string if x != "c" and x != "C"]
     return ("".join(modefied_string))
