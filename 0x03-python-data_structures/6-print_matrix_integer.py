54f017ba5941f2f0cd72f12d376ed1681a927f65
Mostafa Hassan <mostafa813765@gmail.com>
2022-09-27 19:05:23 +0000

    fix for checker

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,7 +3,6 @@ def print_matrix_integer(matrix=[[]]):
     for row in range(len(matrix)):
         for num in range(len(matrix[row])):
             print("{:d}".format(matrix[row][num]), end="")
-            if (num != (len(matrix[row]) - 1)):
+            if num != (len(matrix[row]) - 1):
                 print(" ", end="")
         print("")
-
