c76aaa115054b5f5127997f7c205d54c8d3e3e8c
Mostafa Hassan <mostafa813765@gmail.com>
2022-09-27 23:26:27 +0000

    fix for checker

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    biggest = 0
     if len(my_list) == 0:
         return (None)
+
+    biggest = 0
     for x in my_list:
         if x > biggest:
             biggest = x
+
     return (biggest)
