97231cd408ea56cf97927a39c77a19b1653d4dc1
Mostafa Hassan <mostafa813765@gmail.com>
2022-10-01 18:06:26 +0000

    fix for checker

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 def uppercase(str):
-    upper_word = ''
-    for ch in str:
-        if 96 < ord(ch) < 123:
-            upper_word += chr(ord(ch) - 32)
-        else:
-            upper_word += ch
-    print(upper_word)
+    tmp = list(str)
+    for i in range(len(tmp)):
+        if (ord(tmp[i]) > 96 and ord(tmp[i]) < 123):
+            tmp[i] = chr(ord(tmp[i]) - 32)
+    print("{}".format("".join(tmp)))
