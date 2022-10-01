ae4179c3bd946ec10e4776ac11d121c86c88b88e
Mostafa Hassan <mostafa813765@gmail.com>
2022-10-01 16:30:47 +0000

    fix syntax

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,7 +2,7 @@
 for i in range(10):
     for x in range(10):
         if i == 8 and x == 9:
-            print(89);
+            print(89)
         else:
             if x > i:
                 print("{:d}{:d}".format(i, x), end=", ")
