2d44d6e9d552ab2bf36718c1adfa17a77f7a8887
Mostafa Hassan <mostafa813765@gmail.com>
2022-10-01 19:34:41 +0000

    fix for requirements

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 for char in range(122, 96, -1):
-    if char % 2 == 0:
-        print(chr(char), end="")
-    else:
-        print(chr(char - 32), end="")
+    if char % 2 != 0:
+        char -= 32
+    print("{}".format(chr(char)), end="")
