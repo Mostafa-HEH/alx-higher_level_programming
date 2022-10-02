308cdaa6bdab74489140a3e51f4dc5192dee68cc
Mostafa Hassan <mostafa813765@gmail.com>
2022-10-02 13:40:02 +0000

    print the number of and the list of its arguments

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+from sys import argv
+arg_len = len(argv) - 1
+argument = 'argument' if arg_len == 1 else 'arguments'
+argument += '.' if arg_len == 0 else ':'
+print("{:d} {:s}".format(arg_len, argument))
+if len(argv) > 0:
+    for ar in range(1,len(argv)):
+        print("{:d}: {}".format(ar, argv[ar]))
