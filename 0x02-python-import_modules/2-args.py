ec5053535e72f1ac77c9ec4c87a7815f38d79032
Mostafa Hassan <mostafa813765@gmail.com>
2022-10-02 13:48:20 +0000

    fix for checker

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from sys import argv
-    arg_len = len(argv) - 1
+    import sys
+    arg_len = len(sys.argv) - 1
     argument = 'argument' if arg_len == 1 else 'arguments'
     argument += '.' if arg_len == 0 else ':'
     print("{:d} {:s}".format(arg_len, argument))
-    if len(argv) > 0:
-        for ar in range(1,len(argv)):
-            print("{:d}: {}".format(ar, argv[ar]))
+    if len(sys.argv) > 0:
+        for ar in range(1,len(sys.argv)):
+            print("{:d}: {}".format(ar, sys.argv[ar]))
