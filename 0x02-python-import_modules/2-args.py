be93fac5bad2b0fdbbe3bfca369c6e58aa9ea3d5
Mostafa Hassan <mostafa813765@gmail.com>
2022-10-02 13:43:15 +0000

    make code runs only on main

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-from sys import argv
-arg_len = len(argv) - 1
-argument = 'argument' if arg_len == 1 else 'arguments'
-argument += '.' if arg_len == 0 else ':'
-print("{:d} {:s}".format(arg_len, argument))
-if len(argv) > 0:
-    for ar in range(1,len(argv)):
-        print("{:d}: {}".format(ar, argv[ar]))
+if __name__ == "__main__":
+    from sys import argv
+    arg_len = len(argv) - 1
+    argument = 'argument' if arg_len == 1 else 'arguments'
+    argument += '.' if arg_len == 0 else ':'
+    print("{:d} {:s}".format(arg_len, argument))
+    if len(argv) > 0:
+        for ar in range(1,len(argv)):
+            print("{:d}: {}".format(ar, argv[ar]))
