16736a5868a097720c0b5b78a756042298a2ce0e
Mostafa Hassan <mostafa813765@gmail.com>
2022-10-10 19:29:12 +0000

    divide 2 integers and prints the result

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def safe_print_division(a, b):
+    result = 0
+    try:
+        result = a / b
+    except ZeroDivisionError:
+        result = None
+    finally:
+        if (result != None):
+            print('Inside result: {:.1f}'.format(result))
+        else:
+            print('Inside result: None')
+        return result
--- 0x05-python-exceptions/README.md
+++ 0x05-python-exceptions/README.md
@@ -11,3 +11,4 @@
 Task 0: Write a function that prints x elements of a list.
 Task 1: Write a function that prints an integer with "{:d}".format().
 Task 2: Write a function that prints the first x elements of a list and only integers.
+Task 3: Write a function that divides 2 integers and prints the result.
