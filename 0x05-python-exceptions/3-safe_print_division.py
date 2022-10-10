e3acd921c78c2f49b40af706581ce92fd7eb811d
Mostafa Hassan <mostafa813765@gmail.com>
2022-10-10 19:42:28 +0000

    fix for checker

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -7,8 +7,5 @@ def safe_print_division(a, b):
     except ZeroDivisionError:
         result = None
     finally:
-        if (result != None):
-            print('Inside result: {:.1f}'.format(result))
-        else:
-            print('Inside result: None')
+        print('Inside result: {}'.format(result))
         return result
