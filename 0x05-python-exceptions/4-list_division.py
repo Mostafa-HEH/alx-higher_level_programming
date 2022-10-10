f5b970cf3b508dc64d27bc9bbe2c251ca7915dd2
Mostafa Hassan <mostafa813765@gmail.com>
2022-10-10 20:36:40 +0000

    fix bug - Undefiend variable

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -15,5 +15,5 @@ def list_division(my_list_1, my_list_2, list_length):
             print("out of range")
             result = 0
         finally:
-            results.append(div)
+            results.append(result)
     return results
