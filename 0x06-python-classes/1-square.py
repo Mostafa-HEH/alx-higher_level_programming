585a4a83ff8df4298cdb5b2201c002cf0b021946
Mostafa Hassan <mostafa813765@gmail.com>
2022-10-11 18:22:07 +0000

    fix for checker

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
-"""
-This module defines a Square class and initialise its size
-"""
+"""This module defines a Square class"""
 
 class Square:
     """Square implementation
     """
     def __init__(self, size):
+        """Initialize a new Square.
+
+            Args:
+                size (int): The size of the new square.
+        """
         self.__size = size
