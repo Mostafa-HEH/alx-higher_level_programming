86439598b4f469c9b712dd97cec8e5e5d71df410
Mostafa Hassan <mostafa813765@gmail.com>
2022-10-24 17:05:54 +0000

    updtae print return

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -5,12 +5,12 @@ Write a class BaseGeometry
 
 
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
-Rectangle = __import__('9-rectangle.py').Rectangle
+Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-
     """Define squre class that inhertes Rectangle"""
+
     def __init__(self, size):
         """Init instaltion attributes"""
         self.integer_validator('size', size)
@@ -19,7 +19,7 @@ class Square(Rectangle):
 
     def __str__(self):
         """Print width and height"""
-        return super().__str__()
+        return str("[Square] {}/{}".format(self.__size, self.__size))
 
     def area(self):
         """Return area size"""
