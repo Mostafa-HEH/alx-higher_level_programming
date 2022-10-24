1045a52e581cca315901bd8489d83f669cd066f7
Mostafa Hassan <mostafa813765@gmail.com>
2022-10-24 14:23:13 +0000

    add Public instance method area

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Module 6-base_geometry.py
+Write a class BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """Inilalize BaseGeometry class"""
+
+    def area(self):
+        """Function Raise exception"""
+        raise Exception("area() is not implemented")
