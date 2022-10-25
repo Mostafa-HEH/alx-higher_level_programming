6c18cc4f6dc358dcdc52d4721460410388c88c22
Mostafa Hassan <mostafa813765@gmail.com>
2022-10-25 15:57:11 +0000

    fix docs for checker

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -1,11 +1,18 @@
 #!/usr/bin/python3
-"""Module 12-pascal_triangle
-Create a function def pascal_triangle
+"""Module 14-pascal_triangle.
+Returns a list of lists of integers
+representing the Pascal’s triangle of n.
 """
 
 
 def pascal_triangle(n):
-    """Returns a list of lists of integers"""
+    """Returns the pascal triangle of n.
+
+    Args:
+        - n: size of the triangle (rows)
+
+    Returns: a list of list of integers
+    """
 
     if n <= 0:
         return []
