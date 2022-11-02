b388159dfb8bb8afe25818df64e6e1f02c68469c
Mostafa Hassan <mostafa813765@gmail.com>
2022-11-02 19:57:32 +0000

    make line shorter than 80 litter

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -81,4 +81,6 @@ class Rectangle(Base):
 
     def __str__(self):
         """Return details of rectangle"""
-        return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.x, self.y, self.width, self.height)
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id,
+                                                       self.x, self.y,
+                                                       self.width, self.height)
